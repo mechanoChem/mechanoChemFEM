cee2e056d7d8764f60c4a7b08188e80e64719ebf
wzhenlin <wzhenlin@zhenlins-MacBook-Pro.local>
2018-05-22 13:18:30 -0400

    good

--- doxygen/codeStructure.h
+++ doxygen/codeStructure.h
@@ -3,5 +3,5 @@
  * 
  * The code is divided into three levels: <B>developer level</B>, <B>user level 0</B> and <B>user level 1</B>.
  * The user defined functions is a list of functions that can/must be defined by the user.
- * @image html Codestructure.svg 
+ * @image html Codestructure.png
  */

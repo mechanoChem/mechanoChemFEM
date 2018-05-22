d63f36f0195994f3da3a88682832718fc0514c3e
Zhenlin Wang <wzhenlin@caen-vnc-vm12.engin.umich.edu>
2018-05-22 13:28:48 -0400

    best one for doxygen1.8.5

--- doxygen/codeStructure.h
+++ doxygen/codeStructure.h
@@ -3,5 +3,6 @@
  * 
  * The code is divided into three levels: <B>developer level</B>, <B>user level 0</B> and <B>user level 1</B>.
  * The user defined functions is a list of functions that can/must be defined by the user.
- * @image html Codestructure.png
+\htmlonly <style>div.image img[src="Codestructure"]{width:1000px;}</style> \endhtmlonly 
+ * \image html Codestructure
  */
